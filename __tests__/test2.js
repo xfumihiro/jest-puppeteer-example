9693b999c2f7a126f20b1636b10e808569554e6b
Fumihiro Xue <xfumihiro@gmail.com>
2018-01-06 16:40:48 +0800

    fix: close pages after each test

--- __tests__/test2.js
+++ __tests__/test2.js
@@ -9,6 +9,10 @@ describe(
       await page.goto('https://google.com')
     }, timeout)
 
+    afterAll(async () => {
+      await page.close()
+    })
+
     it('should load without error', async () => {
       let text = await page.evaluate(() => document.body.textContent)
       expect(text).toContain('google')
