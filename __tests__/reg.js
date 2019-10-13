e07850bb61c6b971f8092f8deb7142a0070c9b1d
ivan1krulov <ivan1krulov@gmail.com>
2019-10-13 16:07:43 +0300

    NewUpdate

--- __tests__/reg.js
+++ __tests__/reg.js
@@ -1,15 +1,13 @@
-const puppeteer = require('puppeteer');
+~const puppeteer = require('puppeteer');
 
 (async () => {
 const browser = await puppeteer.launch( {headless:false} );
 const page = await browser.newPage();
 await page.goto ('https://www.tempmailaddress.com/', {waitUntil: 'domcontentloaded'});
-
 await page.waitFor(10000);
 const email = await page.evaluate(() => {
     return document.querySelector ("span#email").innerHTML
 });
-
 await page.goto('https://accounts.yellowpages.com/register');
 await page.type("input[name^='user[first_name]']", "Ivan");
 await page.type("input[name^='user[last_name]']", "Krylov");
@@ -17,7 +15,6 @@ await page.type("input[name^='user[email]']", email);
 await page.type("input[name^='user[new_password]']", "Krylov123123");
 await page.type("input[name^='user[new_password_confirmation]']", "Krylov123123");
 await page.type("input[name^='user[zip_code]']", "90210");
-
 await page.click("input[value^='Join YP!']");
  await browser.close();
 })();
